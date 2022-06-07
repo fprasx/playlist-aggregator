5dc1497fcb577ed2c5683e82b4c6c5b9eef6591a
fprasx <felix725@gmail.com>
2022-06-07 12:44:50 -0400

    changed auth strat to oauth-pkce

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 use futures_util::StreamExt;
 use rspotify::model::PlayableItem::{Episode, Track};
-use rspotify::{
-    prelude::*, scopes, AuthCodeSpotify, Config, Credentials, OAuth, DEFAULT_API_PREFIX,
-    DEFAULT_CACHE_PATH, DEFAULT_PAGINATION_CHUNKS,
-};
+use rspotify::{prelude::*, scopes, AuthCodePkceSpotify, Config, Credentials, OAuth};
 use std::collections::HashSet;
-use std::path::PathBuf;
 
 #[tokio::main]
 async fn main() {
@@ -19,17 +15,14 @@ async fn main() {
     .unwrap();
 
     let config = Config {
-        prefix: DEFAULT_API_PREFIX.to_owned(),
-        cache_path: PathBuf::from(DEFAULT_CACHE_PATH),
-        pagination_chunks: DEFAULT_PAGINATION_CHUNKS,
         token_cached: true,
         token_refreshing: true,
+        ..Default::default()
     };
 
-    let mut spotify = AuthCodeSpotify::with_config(creds, oauth, config);
+    let mut spotify = AuthCodePkceSpotify::with_config(creds, oauth, config);
+    let url = spotify.get_authorize_url(None).unwrap();
 
-    // Obtaining the access token
-    let url = spotify.get_authorize_url(false).unwrap();
     spotify.prompt_for_token(&url).await.unwrap();
 
     let mut tracks = HashSet::<String>::new();
@@ -37,10 +30,10 @@ async fn main() {
     add_tracks_from_playlists(&spotify, &mut tracks).await;
     add_tracks_from_liked_songs(&spotify, &mut tracks).await;
 
-    println!("{:#?}", tracks);
+    println!("You have {} songs!", tracks.len());
 }
 
-async fn add_tracks_from_liked_songs(spotify: &AuthCodeSpotify, tracks: &mut HashSet<String>) {
+async fn add_tracks_from_liked_songs(spotify: &impl OAuthClient, tracks: &mut HashSet<String>) {
     // Note: cannot include the funciton call in the while loop condition!!!
     // It will just return the first track every time, as it's making a new api call every loop iteration
     let mut saved_tracks = spotify.current_user_saved_tracks(None);
@@ -49,7 +42,7 @@ async fn add_tracks_from_liked_songs(spotify: &AuthCodeSpotify, tracks: &mut Has
     }
 }
 
-async fn add_tracks_from_playlists(spotify: &AuthCodeSpotify, tracks: &mut HashSet<String>) {
+async fn add_tracks_from_playlists(spotify: &impl OAuthClient, tracks: &mut HashSet<String>) {
     // Note: cannot include the funciton call in the while loop condition!!!
     // It will just return the first playlist every time, as it's making a new api call every loop iteration
     let mut playlists = spotify.current_user_playlists();
